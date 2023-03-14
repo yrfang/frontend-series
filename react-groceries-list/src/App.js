603bba3b3aae16d90ea3dac913333dde720288f8
Yingru Fang <fyr1116@gmail.com>
2023-03-13 17:22:01 -0700

    Add add item and search item input

--- react-groceries-list/src/App.js
+++ react-groceries-list/src/App.js
@@ -2,44 +2,68 @@ import { useState } from "react";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One bag of washed organic broccoli"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3"
-    }
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")));
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
 
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => {
-      return item.id === id ? { ...item, checked: !item.checked } : item
-    });
+  const setAndSaveItems = (listItems) => {
     setItems(listItems);
     localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   }
 
+  const addItem = (newItem) => {
+    const id = items.length ? items[items.length-1].id + 1 : 1;
+    const curItem = { id, checked: false, item: newItem };
+    const listItems = [...items, curItem];
+    setAndSaveItems(listItems);
+  }
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => {
+      return item.id === id ? { ...item, checked: !item.checked } : item;
+    });
+    setAndSaveItems(listItems);
+  }
+
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem(""); //reset input
+  }
+
+  const filterItem = () => {
+    return items.filter((item) => (
+      item.item.toLowerCase().includes(search.toLowerCase())
+    ));
   }
 
   return (
     <div className="App">
       <Header title="Groceries List" />
-      <Content items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+      <Content
+        items={filterItem()}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
       <Footer length={items.length} />
     </div>
   );
--- react-groceries-list/src/LineItem.js
+++ react-groceries-list/src/LineItem.js
@@ -2,7 +2,7 @@ import { FaTrashAlt } from "react-icons/fa";
 
 const LineItem = ({ item, handleCheck, handleDelete }) => {
   return (
-    <li className="item" key={item.id}>
+    <li className="item">
       <input
           id={item.id}
           type="checkbox"
